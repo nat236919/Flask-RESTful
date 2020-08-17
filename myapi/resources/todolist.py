17331b1fd50b4c1aea9e911b2f759b222d350788
nat236919 <nuttaphat.aruno@gmail.com>
2020-08-17 09:41:34 +0800

    test pop method

--- myapi/resources/todolist.py
+++ myapi/resources/todolist.py
@@ -1,7 +1,8 @@
 """
 Resources - TodoList
 """
-# Import lib
+# Import libs
+from typing import List, Dict, Any
 from flask_restful import Resource, reqparse
 
 # Set-up constant variables
@@ -20,12 +21,26 @@ parser.add_argument('task')
 # TodoList
 # shows a list of all todos, and lets you POST to add new tasks
 class TodoList(Resource):
-    def get(self):
+    def get(self) -> Dict[str, Dict[str, Any]]:
         return TODOS
 
-    def post(self):
+    def post(self) -> Dict[str, Any]:
         args = parser.parse_args()
-        todo_id = int(max(TODOS.keys()).lstrip('todo')) + 1
+        todo_id = self._get_current_id() + 1
         todo_id = 'todo%i' % todo_id
         TODOS[todo_id] = {'task': args['task']}
         return TODOS[todo_id], 201
+    
+    def delete(self) -> None:
+        """ Pop out the latest data """
+        todo_id = self._get_current_id()
+        todo_id = 'todo%i' % todo_id
+        TODOS.pop(todo_id)
+        return '', 204
+    
+    def _get_current_id(self) -> int:
+        """ Get the current id (max) from TODOS """
+        if not TODOS:
+            return 0
+        return int(max(TODOS.keys()).lstrip('todo'))
+        
