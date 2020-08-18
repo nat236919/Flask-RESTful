5981e69371e9e7c01fd801ee45a8a399357c7870
nat236919 <nuttaphat.aruno@gmail.com>
2020-08-18 17:40:51 +0800

    add config

---
+++ myapi/config.py
@@ -0,0 +1,10 @@
+"""
+Environment Configurations
+"""
+DATABASE_CONFIG = {
+    'mongodb_connection_string': 'mongodb+srv://{username}:{password}@{clustername}.1hcgo.azure.mongodb.net/{dbname}?retryWrites=true&w=majority',
+    'clustername': '',
+    'dbname': '',
+    'username': '',
+    'password': ''
+}
