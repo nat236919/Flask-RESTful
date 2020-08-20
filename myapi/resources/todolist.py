1c3ffb2e886dd76d6a47c4c7ad6365e63db88796
nat236919 <nuttaphat.aruno@gmail.com>
2020-08-20 11:20:51 +0800

    add POST method

--- myapi/resources/todolist.py
+++ myapi/resources/todolist.py
@@ -2,19 +2,18 @@
 Resources - TodoList
 """
 # Import libs
+import time
 import pymongo
 from config import DATABASE_CONFIG
 from typing import List, Dict, Any
-from flask_restful import Resource, reqparse
+from flask import jsonify
+from flask_restful import Resource, reqparse, abort
 
 # Set MongoDB connection
 CONNECTION_STRING = DATABASE_CONFIG['mongodb_connection_string'].replace('{username}', DATABASE_CONFIG['username'])\
                                                                 .replace('{password}', DATABASE_CONFIG['password'])\
                                                                 .replace('{clustername}', DATABASE_CONFIG['clustername'])\
                                                                 .replace('{dbname}', DATABASE_CONFIG['dbname'])
-client = pymongo.MongoClient(CONNECTION_STRING)
-db = client[DATABASE_CONFIG['dbname']]
-todolist_collection = db['todolist']
 
 # Parser
 parser = reqparse.RequestParser()
@@ -24,15 +23,46 @@ parser.add_argument('task')
 # TodoList
 # shows a list of all todos, and lets you POST to add new tasks
 class TodoList(Resource):
-    def get(self) -> Dict[str, Dict[str, Any]]:
-        return TODOS
+    def __init__(self):
+        self.client = pymongo.MongoClient(CONNECTION_STRING)
+        self.db = self.client[DATABASE_CONFIG['dbname']]
+        self.todolist_collection = self.db['todolist']
+        self.data_schema = {
+            '_id': '',
+            'task': '',
+            'is_complete': False,
+            'created_ts': ''
+        }
+
+    def get(self) -> List[Dict[str, Any]]:
+        """ Get all the documents from the collection """
+        res = self.todolist_collection.find({})
+        return jsonify([doc for doc in res])
 
     def post(self) -> Dict[str, Any]:
+        """ Add a document onto the collection
+            Schema:
+                {
+                    "task": "example task"
+                }
+        """
         args = parser.parse_args()
-        todo_id = self._get_current_id() + 1
-        todo_id = 'todo%i' % todo_id
-        TODOS[todo_id] = {'task': args['task']}
-        return TODOS[todo_id], 201
+        if not args:
+            return abort(400, message='data was not provided')
+    
+        all_docs = [doc for doc in self.todolist_collection.find({})]
+        cur_id = max(all_docs, key=lambda doc: doc['_id'])['_id'] # Get current document id
+        try:
+            data_schema = self.data_schema.copy()
+            data_schema['_id'] = cur_id + 1
+            data_schema['task'] = args['task']
+            data_schema['created_ts'] = time.time()
+            self.todolist_collection.insert_one(data_schema.copy())
+
+        except Exception as e:
+            return abort(400, message=e)
+
+        return data_schema, 201
     
     def delete(self) -> None:
         """ Pop out the latest data """
