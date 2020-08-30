7276e9fff0360b0c9266d8a1a87411f87a4226ea
Nuttaphat <nuttaphat.aruno@gmail.com>
2020-08-30 16:28:16 +0800

    add User resource and GET method

--- api/app.py
+++ api/app.py
@@ -9,6 +9,7 @@ from flask_restful import Api
 from config import APP_CONFIG, DATABASE_CONFIG
 from resources.todo import Todo
 from resources.todolist import TodoList
+from resources.user import User
 
 # Initiate APP and API
 app = Flask(__name__)
@@ -28,6 +29,7 @@ db = client[DATABASE_CONFIG['dbname']]
 # Set-up Resources
 api.add_resource(TodoList, '/todos', resource_class_kwargs={'todolist_collection': db['todolist']})
 api.add_resource(Todo, '/todos/<string:todo_id>', resource_class_kwargs={'todolist_collection': db['todolist']})
+api.add_resource(User, '/users', resource_class_kwargs={'user_collection': db['users']})
 
 
 # Run app
--- api/resources/user.py
+++ api/resources/user.py
@@ -11,3 +11,36 @@ from flask_restful import Resource, abort, reqparse
 # Parser
 parser = reqparse.RequestParser()
 parser.add_argument('data', type=dict, required=True)
+
+
+# User (admin)
+class User(Resource):
+    def __init__(self, user_collection):
+        self.user_collection = user_collection
+        self.data_schema = {
+            '_id': '',
+            'username': '',
+            'password': '',
+            'is_admin': False,
+            'created_ts': ''
+        }
+
+    def get(self) -> List[Dict[str, Any]]:
+        """ Get all users from the collection """
+        res = self._get_all_docs()
+        return jsonify(res)
+    
+    def _get_all_docs(self) -> List[Dict[str, Any]]:
+        """ Get all documents in the collection """
+        return [doc for doc in self.user_collection.find({})]
+
+    def _get_current_id(self) -> int:
+        """ Get the current id (max) from TODOS """
+        all_docs = self._get_all_docs()
+        if not all_docs:
+            return 0
+
+        # Get current document id
+        cur_id = int(max(all_docs, key=lambda doc: doc['_id'])['_id'])
+
+        return cur_id
